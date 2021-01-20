bfb8c6428322009e1d1f060a5668db6200020f47
keshey <75967167+keshey@users.noreply.github.com>
2021-01-20 13:07:19 +1000

    Version 1.1 Alpha release
    added additional condition to handle spaces in single cell having multiple disease codes and replaced by comma and then split it

--- split.py
+++ split.py
@@ -2,7 +2,11 @@ import pandas as pd
 
 
 def split_function(file_path, column):
-    df_read_data = pd.read_excel(file_path)  # reading the data from file in filepath to dataframe
+    df_read_data = pd.read_excel(file_path)  # reading the data from file in file-path to data-frame
+    df_read_data[column] = df_read_data[column].str.strip()
+    df_read_data[column] = df_read_data[column].str.replace(" ", ",")
+    df_read_data[column] = df_read_data[column].str.split(",")
+    df_read_data = df_read_data.apply(pd.Series.explode)
     condition_values = df_read_data[column].unique()
     for value in condition_values:
         df_split_data = df_read_data[df_read_data[column] == value]
@@ -11,8 +15,8 @@ def split_function(file_path, column):
 
 
 def main():
-    for i in range(0, 1):
-        file_path = input("please enter excel file path: ")
+    file_path = input("please enter excel file path: ")
+    for i in range(2):
         column = input("please enter column name that need to be split: ")
         split_function(file_path, column)
 
